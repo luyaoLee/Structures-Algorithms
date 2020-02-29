88f58767b5939232c21ba69fed8ead02b3dcefcb
luyao <luyao_lee@163.com>
2020-02-29 20:24:57 +0800

    选择排序

---
+++ selection-sort/selection-sort.js
@@ -0,0 +1,31 @@
+const { defaultCompare, Compare, swap } = require('../common/utils')
+
+/**
+ * 思路：选择排序就是找到每次循环的最小值并把它放在第一位
+ * @param {*} array 
+ * @param {*} compareFn 
+ */
+function selectionSort(array, compareFn = defaultCompare) {
+  let copyArr = array.slice()
+  const { length } = copyArr
+
+  // 因为排序到倒数第二位时，此时顺序已经确定了。所以循环次数是length - 1
+  for (let i = 0; i < length - 1; i++) {
+    let indexMin = i
+    for (let j = i; j < length; j++) {
+      if (compareFn(copyArr[indexMin], copyArr[j]) === Compare.BIGGER_THAN) {
+        indexMin = j
+      }
+    }
+    if (indexMin !== i ) {
+      swap(copyArr, i, indexMin)
+    }
+  }
+  return copyArr
+}
+
+module.exports = selectionSort
+
+const arr = [2, 23, 6, 78, 4, 3]
+
+console.log(selectionSort(arr))
